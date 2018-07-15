a96e709c4b9ead3207387e8d02f1fab8ff6dc6a8
Fulton Zink <joeyzink@gmail.com>
2018-07-15 11:51:18 -0700

    Clean up styling.

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import styles from '../styles/table.scss';
 
 function renderHeaders(headers) {
-    return headers.map((header, index) => <th scope="col" key={index}>{header}</th>);
+    return headers.map((header, index) => <th className={styles.header} scope="col" key={index}>{header}</th>);
 }
 
 const Headers = (props) => {
     return (
         <thead>
-            <tr className="Headers">
+            <tr className={`Headers ${styles.header}`}>
                 {renderHeaders(props.headers)}
             </tr>
         </thead>
